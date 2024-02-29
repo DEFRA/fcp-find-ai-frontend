c8677972d9352b0e4b8ed2783a7e17282f8ab54e
Mohammed Foysal <MohammedFoysal@users.noreply.github.com>
2024-02-29 17:29:19 +0000

    Single page app

--- app/frontend/css/index.js
+++ app/frontend/css/index.js
@@ -1,3 +1,17 @@
 import { initAll } from 'govuk-frontend'
 import './application.scss'
 initAll()
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const sendButton = document.getElementById('sendButton')
+  const sendForm = document.getElementById('sendForm')
+
+  if (sendButton) {
+    sendButton.onclick = (event) => {
+      event.preventDefault()
+      sendButton.disabled = true
+
+      sendForm.submit()
+    }
+  }
+})
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,22 +1,61 @@
+const axios = require('axios')
 const config = require('../config')
 
 module.exports = {
-  method: 'GET',
+  method: ['GET', 'POST'],
   path: '/',
   options: {
-    handler: (request, h) => {
+    handler: async (request, h) => {
+      console.log('Loaded home')
       let validationError = false
 
       if (request.query?.error === 'validation') {
         validationError = true
       }
 
-      return h.view('home', {
-        envTest: config.envTest,
-        fundingFarmingApiUri: config.fundingFarmingApiUri,
-        appInsightsKey: config.appInsightsKey,
-        validationError
-      })
+      const input = request.payload?.input
+
+      if (!input) {
+        return h.view('home', {
+          envTest: config.envTest,
+          fundingFarmingApiUri: config.fundingFarmingApiUri,
+          appInsightsKey: config.appInsightsKey,
+          validationError
+        })
+      }
+
+      try {
+        const url = `${config.fundingFarmingApiUri}/answer_query`
+
+        console.log(`Performing POST request: ${url}`)
+        const axiosConfig = {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-functions-key': config.fundingFarmingApiKey
+          }
+        }
+
+        const response = await axios.post(
+          url,
+          {
+            input
+          },
+          axiosConfig
+        )
+
+        const messages = [response.data]
+
+        return h.view('home', {
+          envTest: config.envTest,
+          fundingFarmingApiUri: config.fundingFarmingApiUri,
+          appInsightsKey: config.appInsightsKey,
+          validationError,
+          messages
+        })
+      } catch (error) {
+        console.error(error)
+        throw error
+      }
     }
   }
 }
