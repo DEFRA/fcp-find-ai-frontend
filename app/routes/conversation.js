fe3ed3c756036c11b72919ef42e8871d1238dadf
Stan <stanhuseletov@gmail.com>
2024-07-16 16:27:28 +0100

    table return

--- app/routes/conversation.js
+++ app/routes/conversation.js
@@ -168,7 +168,43 @@ module.exports = [
         logger.info('Response generated in ' + ((endTime.getTime() - startTime.getTime()) / 1000).toFixed(2) + ' seconds')
       }
 
-      return h.response(responses).code(200)
+      // return response as an html table
+      const table = responses.map((response) => {
+        return `<tr><td>${response.input}</td><td>${response.response}</td><td>${response.responseDuration}</td></tr>`
+      }).join('')
+
+      const html = `
+        <html>
+          <head>
+            <style>
+              table {
+                border-collapse: collapse;
+                width: 100%;
+              }
+              th, td {
+                border: 1px solid black;
+                padding: 8px;
+                text-align: left;
+              }
+              th {
+                background-color: #f2f2f2;
+              }
+            </style>
+          </head>
+          <body>
+            <table>
+              <tr>
+                <th>Input</th>
+                <th>Response</th>
+                <th>Duration</th>
+              </tr>
+              ${table}
+            </table>
+          </body>
+        </html>
+      `
+
+      return h.response(html).type('text/html')
     }
   }
 ]
