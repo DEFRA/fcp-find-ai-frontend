666e8f2fb61c4b0b363163adfa20663e274df916
Stan <stanhuseletov@gmail.com>
2024-07-17 12:07:15 +0100

    version incrememnt

--- app/config/index.js
+++ app/config/index.js
@@ -69,7 +69,7 @@ const schema = Joi.object({
 const config = {
   env: process.env.NODE_ENV,
   appInsightsKey: process.env.APPINSIGHTS_CONNECTIONSTRING,
-  version: '0.1.40',
+  version: '0.1.42',
   logLevel: process.env.LOG_LEVEL || 'error',
 
   auth: {
